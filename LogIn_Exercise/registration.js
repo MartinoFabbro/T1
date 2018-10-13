8edc4f4d524455367eb2c407868d1c9727d76bd5
MartinoFabbro <martino.fabbro@gmail.com>
2018-10-13 13:50:53 +0200

    fixed problems with registration form, now you can not registrer if you use white spaces

--- LogIn_Exercise/registration.js
+++ LogIn_Exercise/registration.js
@@ -1,13 +1,22 @@
 var submit = document.getElementById("submit");
 
 function getInfo() {
+
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     var password1 = document.getElementById("password1").value;
- if (!password || !username || !password1) {
+
+
+    if (!password || !username || !password1) {
      alert("idiot");
      return;
- }else if (password === password1) {
+ }else if ((password.length < 6) || (username.length <6)) {
+    alert("min 6 characters for user and pass dork");
+    return;
+ } else if ((username.indexOf(" ") !== -1) || (password.indexOf(" ") !== -1)) {
+     alert("are you stupid or just pretending? No spaces");
+     return;
+ } else if (password === password1) {
     localStorage.setItem('username', username);
     localStorage.setItem('password', password);
     alert("Registration Succesfull. You will now be redirected");
