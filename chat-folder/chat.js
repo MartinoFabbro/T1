1c8dba0cd06bc17cf933254700c2297a77e14403
maja18ai <43538008+maja18ai@users.noreply.github.com>
2018-11-24 16:25:54 +0100

    Update chat.js

--- chat-folder/chat.js
+++ chat-folder/chat.js
@@ -3,7 +3,7 @@
 //different socket.io than the backend
 var socket = io.connect("http://localhost:8080");
 
-//implementing variables from index.html
+//implementing variables from chat.html
 var message = document.getElementById("message");
 username = document.getElementById("username"),
 button = document.getElementById("send"),
